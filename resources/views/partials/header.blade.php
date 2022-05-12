8249b6ea73ad78be271434586450b4a8fe626d47
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-05-12 23:21:31 +0200

    add navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,9 @@
 <header>
-    <h1 class="text-center">header</h1>
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container text-center">
+            <a class="navbar-brand h1 fs-3 text-capitalize" href="#">i nostri film</a>
+          </div>
+        </nav>
+      </div>
 </header>
