6c168dc90be47842244b8d73cf6fb612bf85ae81
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-05-12 16:42:35 +0200

    add movieController

---
+++ app/Http/Controllers/movieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class movieController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('movies.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.base');
-});
+Route::get('/', "movieController@index");
